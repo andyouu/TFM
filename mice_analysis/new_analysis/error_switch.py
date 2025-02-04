7b4d6bbbdc2fc9917a54a5141d8e6b769f81339a
anyouu <marcandueza@gmail.com>
2025-02-04 09:32:26 +0100

    Prob_switching

--- mice_analysis/new_analysis/error_switch.py
+++ mice_analysis/new_analysis/error_switch.py
@@ -46,14 +46,44 @@ def number_fails_plot(df, n):
         num_rewrd = 0
         num_fail = 0
         i += 1 
-
-    print(count)
     plt.plot(range(1, n + 1), count[:, 0], marker='o')
     plt.xlabel('Number of Consecutive Rewards')
     plt.ylabel('Average Number of Failures')
     plt.title('Average Failures After Consecutive Rewards')
     plt.grid(True)
     plt.show()           
+
+
+
+def prob_switch(df, n):
+    count = np.zeros((n, 3))  # [total_occurrences, switches, probability]
+
+    i = 0
+    while i < len(df) - 1:
+        j = 0
+        while (i + j < len(df)) and (df.loc[i + j, 'outcome'] == 'incorrect'):
+            j += 1
+
+        if j > 0:
+            if j < n:
+                count[j][0] += 1 
+                if (i + j < len(df)) and (df.loc[i + j, 'choice'] != df.loc[i + j - 1, 'choice']):
+                    count[j][1] += 1
+
+        i += max(j, 1)
+
+
+    for k in range(n):
+        if count[k][0] > 0:
+            count[k][2] = count[k][1] / count[k][0]
+
+    # Plot results
+    plt.plot(range(1, n + 1), count[:, 2], marker='o')
+    plt.xlabel('Number of Consecutive Failures')
+    plt.ylabel('Probability of Switching')
+    plt.title('Probability of Switching Given Number of Errors')
+    plt.grid(True)
+    plt.show()    
 
 
 
@@ -80,5 +110,6 @@ if __name__ == '__main__':
     new_df.loc[(new_df['outcome_bool'] == 0) & (new_df['side'] == 'left'), 'choice'] = 'right'
     new_df.loc[(new_df['outcome_bool'] == 1) & (new_df['side'] == 'right'), 'choice'] = 'right'
     new_df['choice'].fillna('other', inplace=True)
-    n_max_rewards = 6
-    number_fails_plot(new_df,n_max_rewards)
+    n_max_rewards = 10
+    #number_fails_plot(new_df,n_max_rewards)
+    prob_switch(new_df.dropna(subset=['outcome']).reset_index(drop=True),n_max_rewards)
